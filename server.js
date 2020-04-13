f40903dee06df440c17c0a3cf899dd3445598cb0
corentin.ravet <corentin.ravet@eemi.com>
2020-04-13 15:36:08 +0200

    Exercice 7

--- server.js
+++ server.js
@@ -3,52 +3,85 @@ const app = express();
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
-let path = "réponses.json";
+const FICHIER_REPONSES = "réponses.json";
 
-fs.readFile(path, "utf-8", (err, data) => {
-  if (err) throw err;
-  var info = JSON.parse(data);
+console.log("Lecture des réponses depuis", FICHIER_REPONSES);
 
-  app.use(bodyParser.json());
-
-  app.get("/", function (req, res) {
-    res.send("Hello World");
+function readFileAsPromised(file, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(FICHIER_REPONSES, "utf8", (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
   });
+}
 
-  app.get("/hello", function (req, res) {
-    if (req.query.nom === undefined) {
-      res.send("Quel est votre nom ?");
-    } else {
-      res.send("Bonjour, " + req.query.nom + " !");
-    }
-  });
-
-  app.post("/chat", function (req, res) {
-    let request = req.body.msg;
-    if (request === "météo") {
-      res.send("Il fait beau");
-    } else if (request === "ville") {
-      res.send("Nous sommes à Paris");
-    } else if (request != -1) {
-      if (request.search(" = ") == -1) {
-        if (info[request] == undefined) {
-          res.send("Je ne connais pas demain...");
-        } else if (info[request] != undefined) {
-          res.send(request + ": " + info[request]);
-        }
+function writeFileAsPromised(file, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        reject(err);
       } else {
-        var response = request.split(" = ");
-        info[response[0]] = response[1];
-        let data = JSON.stringify(info);
-        fs.writeFile(path, data, function (err) {
-          if (err) return err;
-          res.send("Merci pour cette information !");
-        });
+        resolve();
       }
-    }
+    });
   });
+}
 
-  app.listen(process.env.PORT || 3000, function () {
-    console.log("Example app listening on port " + process.env.PORT + " !");
+const promesseReponses = readFileAsPromised(FICHIER_REPONSES, "utf8");
+
+promesseReponses
+  .catch((err) => {
+    console.error("echec de lecture du fichier:", err);
+    process.exit();
+  })
+  .then((data) => {
+    var info = JSON.parse(data);
+
+    app.use(bodyParser.json());
+
+    app.get("/", function (req, res) {
+      res.send("Hello World");
+    });
+
+    app.get("/hello", function (req, res) {
+      if (req.query.nom === undefined) {
+        res.send("Quel est votre nom ?");
+      } else {
+        res.send("Bonjour, " + req.query.nom + " !");
+      }
+    });
+
+    app.post("/chat", function (req, res) {
+      let request = req.body.msg;
+      if (request === "météo") {
+        res.send("Il fait beau");
+      } else if (request === "ville") {
+        res.send("Nous sommes à Paris");
+      } else if (request != -1) {
+        if (request.search(" = ") == -1) {
+          if (info[request] == undefined) {
+            res.send("Je ne connais pas demain...");
+          } else if (info[request] != undefined) {
+            res.send(request + ": " + info[request]);
+          }
+        } else {
+          var response = request.split(" = ");
+          info[response[0]] = response[1];
+          let data = JSON.stringify(info);
+          writeFileAsPromised(FICHIER_REPONSES, data)
+            .then(() => {
+              res.send("Merci pour cette information !");
+            })
+            .catch((err) => {
+              console.error(err);
+              res.send("Erreur... Reessayez plus tard.");
+            });
+        }
+      }
+    });
+
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Example app listening on port " + process.env.PORT + " !");
+    });
   });
-});
